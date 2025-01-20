df861dc5760dd2b7cbdad725f656a1c71f06f958
Xolisa Nunge <xolisa@flume.co.za>
2025-01-20 22:07:57 +0200

    adding fields to the page , that make up majority of the project , just without the logic

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,20 +1,25 @@
-import { Input , Button , Checkbox } from '@headlessui/react'
-import { useState } from 'react'
+import { Input , Button  } from '@headlessui/react'
 
 
 function App(){
-  const [enabled, setEnabled] = useState(false)
-
   return (
     <>
+    <div className="container">
       <div className="taskInput">
         <Input name="task" type="text" className="py-2 px-4"/>
         <Button className="rounded bg-sky-600 py-2 px-4 text-sm text0white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">Add Task</Button>
       </div>
       <div className="taskList">
-      
-    <h3 className="task">Start learning React</h3>
+         <ul>
+          <span>
+            <input type="checkbox" name="taskButton" />
+          </span>
+          <span>
+            <li>Start learning react now ma'am</li>
+          </span>
+         </ul>
       </div>
+    </div>
     </>
   )}
 
