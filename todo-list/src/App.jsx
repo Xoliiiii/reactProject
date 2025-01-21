383264b9946ed03afa2ae6e1a206e49a16cb07a2
Xolisa Nunge <xolisa@flume.co.za>
2025-01-21 16:26:21 +0200

    Styling the Todo list components

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,21 +1,23 @@
 import { Input , Button  } from '@headlessui/react'
+import './App.css'
 
 
 function App(){
   return (
     <>
-    <div className="container">
-      <div className="taskInput">
-        <Input name="task" type="text" className="py-2 px-4"/>
-        <Button className="rounded bg-sky-600 py-2 px-4 text-sm text0white data-[hover]:bg-sky-500 data-[active]:bg-sky-700">Add Task</Button>
+    <div className="container bg-pink-200 py-20 font-sans rounded-md shadow-2xl">
+      <p className='text-4xl py-4 text-white'>Todo List</p>
+      <div className="taskInput round ">
+        <Input name="task" type="text" className="py-2 px-8 mt-3 mr-4"/>
+        <Button className="rounded bg-pink-600 py-2 px-8 text-sm text-white data-[hover]:bg-pink-500 data-[active]:bg-pink-700" >Add Task</Button>
       </div>
       <div className="taskList">
          <ul>
           <span>
-            <input type="checkbox" name="taskButton" />
+            <Input type="checkbox" name="taskButton" />
           </span>
           <span>
-            <li>Start learning react now ma'am</li>
+            <li>Start learning react now maam</li>
           </span>
          </ul>
       </div>
