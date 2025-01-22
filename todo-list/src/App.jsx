3c73a8aac79e4a8231984fb624350fd6a347431e
Xolisa Nunge <xolisa@flume.co.za>
2025-01-22 14:34:33 +0200

    Adding logic to the application to array the list of tasks that a user inputs

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,23 +1,46 @@
-import { Input , Button  } from '@headlessui/react'
-import './App.css'
+import { Input } from '@headlessui/react';
+import './App.css';
+import Taskbutton from './components/Taskbutton';
+import { useState } from "react";
+import UserInput from './components/UserInput'
+
 
 
 function App(){
+  //I want the list to be empty , and the state should change based on what the user has inputted into the label
+  const [tasks , setTask] = useState([])
+
+  const userTask = (e) => {
+    setTask(e.target.value)
+  }
+
+  const handleEvent = (e) => {
+    e.preventDefault();
+   setTask(...tasks,task)
+    
+  }
+
+
+
   return (
     <>
     <div className="container bg-pink-200 py-20 font-sans rounded-md shadow-2xl">
       <p className='text-4xl py-4 text-white'>Todo List</p>
-      <div className="taskInput round ">
-        <Input name="task" type="text" className="py-2 px-8 mt-3 mr-4"/>
-        <Button className="rounded bg-pink-600 py-2 px-8 text-sm text-white data-[hover]:bg-pink-500 data-[active]:bg-pink-700" >Add Task</Button>
-      </div>
+        <div className="taskInput round ">
+          <UserInput onChange={userTask} value={tasks}/>
+          <Taskbutton onClick={handleEvent}/>
+        </div>
       <div className="taskList">
          <ul>
           <span>
-            <Input type="checkbox" name="taskButton" />
+            <Input type="checkbox" name="taskCheckbox"/>
           </span>
           <span>
-            <li>Start learning react now maam</li>
+            <ul>
+            {tasks.map((task, index) => (
+      <li key={index}>{task}</li>
+    ))}
+            </ul>
           </span>
          </ul>
       </div>
